a379f80428de02d96871fc45e659936c3e530ef0
Hariharan <hariharan9594p@gmail.com>
2022-02-07 23:04:29 -0800

    second commit

---
+++ blog/admin.py
@@ -0,0 +1,7 @@
+from pyexpat import model
+from urllib import request
+from django.contrib import admin
+from blog.models import Post
+# Register your models here.
+
+admin.site.register(Post)
---
+++ blog/views.py
@@ -0,0 +1,35 @@
+import imp
+from django.shortcuts import render
+from blog.models import Post
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+
+from blog.forms import UserRegisterForm
+
+
+class UserLogin(LoginView):
+    template_name = 'blog/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+    
+    def get_success_url(self):
+        return reverse_lazy('blog-home')
+
+# Create your views here.
+def home(request):
+    post = Post.objects.all()
+    return render(request, 'blog/home.html',{'posts':post})
+
+def about(request):
+    return render(request, 'blog/about.html')
+
+class RegisterPage(FormView):
+    template_name = 'blog/register.html'
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('blog-home')
+
+    def form_valid(self, form):
+        form.save()
+        return super(RegisterPage, self).form_valid(form)
